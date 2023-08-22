6b366b5ee10b078abb9a9bb69244e59163552ba2
VaibhavSharma1998 <83278317+VaibhavSharma1998@users.noreply.github.com>
2023-08-22 10:13:06 +0530

    connected to mongodb database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,8 @@
 exports.getAllProducts = (req,res)=>{
   res.status(200).json({
     status:'success',
-    message:'I dont know why it is not working'
+    message:'I dont know why it is not working',
+    update:'Aa gya bhai aub mahnat karni hai aaj and try to made api today'
   })
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
+const mongoose = require('mongoose');
+
 const app = require('./app')
 const dotenv = require('dotenv')
 
 dotenv.config({path:'backend/config/config.env'})
 
+const DB_URI = process.env.DATABASE.replace('<password>',process.env.PASSWORD_DATABASE)
 
-
+mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+       
+      })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas 😎 😂');
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB Atlas:', error.message);
+  });
+// console.log(DB_URI)
 // LISTEN
 
 app.listen(process.env.PORT,()=>{
